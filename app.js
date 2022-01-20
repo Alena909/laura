d4d71baa4d2a6ceb2f85daa637fd560694642218
Yelena Guthrie <yelena.guthrie@gmail.com>
2022-01-20 17:57:09 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/about", (req, res) => {
+  res.render("pages/about");
+});
+
+app.get("/episodes", (req, res) => {
+  res.render("pages/episodes");
+});
+
+app.get("/guests", (req, res) => {
+  res.render("pages/guests");
+});
+
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
+
+app.listen(3000, () => {
+  console.log("server is running");
+});
